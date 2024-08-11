3c8fe2b973cf39c021557108f43eebef08c9c193
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2024-08-11 20:02:10 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+import os, asyncio
+from configs import *
+from aiohttp import web
+from logger import logger
+from pyrogram import Client
+from utilities import web_server, ping_server
+
+class ShortnerBot(Client):
+    def __init__(self):
+        super().__init__(
+            "shortner_bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="plugins"),
+            workers=100
+        )
+
+    async def start(self):
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        ba = "0.0.0.0"
+        port = int(os.getenv("PORT", 8080))
+        await web.TCPSite(app, ba, port).start()
+        await super().start()
+        logger.info("Bot started successfully..")
+        asyncio.create_task(ping_server())
+
+    async def stop(self, *args):
+        await super().stop()
+        logger.info("Bot stepped!")
+
+
+if __name__ == '__main__':
+    ShortnerBot().run()
---
+++ configs.py
@@ -0,0 +1,24 @@
+from os import getenv as genv
+
+API_ID = genv("API_ID", "")
+API_HASH = genv("API_HASH", "")
+BOT_TOKEN = genv("BOT_TOKEN", "")
+BASE_URL = genv("BASE_URL", "")
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "")
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "")
+DATABASE_URL = genv("DATABASE_URL", "")
+START_TXT = '''<b>Hᴇʟʟᴏ {}, I Aᴍ Sʜᴏʀᴛɴᴇʀ Bᴏᴛ!.
+๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀᴛ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴩɪ.
+๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Hᴇʟᴩ Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.</b>'''
+HELP_TXT = '''Sᴇɴᴅ ᴍᴇ ᴀɴʏ ʟɪɴᴋ ɪ ᴡɪʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ᴛᴏ sʜᴏʀᴛ ʟɪɴᴋ ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴘɪ
+ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ᴛʜᴀᴛ ʟɪɴᴋ ᴀɴᴅ ᴇᴀʀɴ ᴍᴏɴᴇʏ.
+sᴇɴᴅ /set_shortner ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ'''
+ABOUT_TXT = '''<b>╔════❰ Sʜᴏʀᴛɴᴇʀ Bᴏᴛ ❱═══❍
+║ ┏━━━━━━━━━❥
+║ ┣ Mʏ ɴᴀᴍᴇ -> {}
+║ ┣ Mʏ Oᴡɴᴇʀ -> @iSmartBoii_Ujjwal
+║ ┣ Uᴘᴅᴀᴛᴇꜱ -> <a href="tg://resolve?domain=">••Bᴏᴛs••</a>
+║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> <a href="tg://resolve?domain="> Bᴏᴛs Sᴜᴩᴩᴏʀᴛ</a>
+║ ┣ ๏ Cʜᴇᴄᴋ ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ.
+║ ┗━━━━━━━━━❥
+╚═════❰ @ ❱═════❍</b>'''

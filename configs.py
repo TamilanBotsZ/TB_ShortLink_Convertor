6bfcd7829b2298f08feac950ffc797d3e85b4908
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2024-08-12 18:42:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ API_ID = genv("API_ID", "")
 API_HASH = genv("API_HASH", "")
 BOT_TOKEN = genv("BOT_TOKEN", "")
 BASE_URL = genv("BASE_URL", "")
-SUPPORT_GROUP = genv("SUPPORT_GROUP", "")
-UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "")
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "TamilanBotsz_support")
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "Tamilan_Botsz")
 DATABASE_URL = genv("DATABASE_URL", "")
 START_TXT = '''<b>Hᴇʟʟᴏ {}, I Aᴍ Sʜᴏʀᴛɴᴇʀ Bᴏᴛ!.
 ๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀᴛ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴩɪ.
